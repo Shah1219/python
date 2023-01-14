4e6196a739317bf4a52b1992ddf37d4784f39b13
shahryar <shahryar1219@gmail.com>
2023-01-14 20:25:52 +0500

    loops

---
+++ Day 1/loops.py
@@ -0,0 +1,24 @@
+# while loops
+
+# x = 1
+# while (x<=10):
+#     print(x)
+#     x = x+1
+
+# for loops
+
+# table of 2 with for
+
+# for x in range (1,11):
+#     print("2 X ",x, " = ", x*2)
+
+dayss = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+for d in dayss:
+    print (d)
+
+days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+for d in days:
+    if d == "Saturday" : break
+    print (d)
